58bcfdccb1f39ff84e17ada3c54c804118c36442
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-06-02 09:33:30 +0200

    Test for nonexistent version setter

--- src/test/java/net/smartcosmos/dto/relationships/RelationshipCreateTest.java
+++ src/test/java/net/smartcosmos/dto/relationships/RelationshipCreateTest.java
@@ -2,16 +2,31 @@ package net.smartcosmos.dto.relationships;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.*;
 
 public class RelationshipCreateTest {
 
     @Test
-    public void thatVersionIsSet() {
+    public void thatRelationshipCreateVersionIsSet() {
         RelationshipCreate relationshipCreate = RelationshipCreate.builder().build();
 
         assertNotNull(relationshipCreate.getVersion());
         assertEquals(1, relationshipCreate.getVersion());
+    }
+
+    /**
+     * This actually tests if Lombok is properly used.
+     */
+    @Test
+    public void thatRelationshipCreateVersionHasNoSetter() {
+        Method getVersion = null;
+        try {
+            getVersion = RelationshipCreate.class.getDeclaredMethod("setVersion", int.class);
+        } catch (NoSuchMethodException e) {
+            // that's what we expect
+        }
+        assertNull(getVersion);
     }
 }
