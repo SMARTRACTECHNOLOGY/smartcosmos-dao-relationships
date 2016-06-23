032c6094992c2476c68f5c183410f3fdf6607fb7
Jan Bauer <jan.bauer@smartrac-group.com>
2016-06-23 16:38:23 +0200

    javadoc updated

--- src/main/java/net/smartcosmos/dao/relationships/RelationshipDao.java
+++ src/main/java/net/smartcosmos/dao/relationships/RelationshipDao.java
@@ -11,31 +11,30 @@ import java.util.Optional;
 public interface RelationshipDao {
 
     /**
-     * Creates a relationship in the realm of a given account.
+     * Creates a relationship in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param createRelationship the relationship to create
      * @return an {@link RelationshipResponse} instance for the created object
      * @throws ConstraintViolationException if the {@link RelationshipCreate} violates constraints enforced by the persistence service
      */
     Optional<RelationshipResponse> create(
-            String tenantUrn,
-            RelationshipCreate createRelationship)
-        throws ConstraintViolationException;
+        String tenantUrn,
+        RelationshipCreate createRelationship) throws ConstraintViolationException;
 
      /**
-     * Finds a relationship matching a specified URN in the realm of a given account.
+     * Finds a relationship matching a specified URN in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param urn the relationship's system-assigned URN
      * @return the {@link RelationshipResponse} instance for the retrieved relationship or {@code empty} if the relationship does not exist
      */
     Optional<RelationshipResponse> findByUrn(String tenantUrn, String urn);
 
     /**
-     * Finds a specific relationship between entities with a specified relationshipType in the realm of a given account.
+     * Finds a specific relationship between entities with a specified relationshipType in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param sourceType the entity reference relationshipType
      * @param sourceUrn the reference entity's system-assigned URN
      * @param targetType the entity reference relationshipType of the related entity
@@ -44,33 +43,33 @@ public interface RelationshipDao {
      * @return the {@link RelationshipResponse} instance for the retrieved relationship or {@code empty} if the relationship does not exist
      */
     Optional<RelationshipResponse> findSpecific(
-            String tenantUrn,
-            String sourceType,
-            String sourceUrn,
-            String targetType,
-            String targetUrn,
-            String relationshipType);
+        String tenantUrn,
+        String sourceType,
+        String sourceUrn,
+        String targetType,
+        String targetUrn,
+        String relationshipType);
 
     /**
-     * Deletes a relationship matching a specified URN in the realm of a given account.
+     * Deletes a relationship matching a specified URN in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param urn the relationship's system-assigned URN
      * @return a list of {@link RelationshipResponse} instances for the deleted relationship entities
      */
     List<RelationshipResponse> delete(String tenantUrn, String urn);
 
     /**
-     * Finds all relationships between specified entities in the realm of a given account.
+     * Finds all relationships between specified entities in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param sourceType the entity reference relationshipType
      * @param sourceUrn the reference entity's system-assigned URN
      * @param targetType the entity reference relationshipType of the related entity
      * @param targetUrn the related reference entity's system-assigned URN
      * @param page the number of the results page
      * @param size the size of a results page
-     * @return a list of matching {@link RelationshipResponse} instances for the retrieved relationships
+     * @return a page of matching {@link RelationshipResponse} instances for the retrieved relationships
      */
     Page<RelationshipResponse> findBetweenEntities(
         String tenantUrn,
@@ -82,15 +81,16 @@ public interface RelationshipDao {
         Integer size);
 
     /**
-     * Finds all relationships for a given reference entity matching a specified relationshipType in the realm of a given account.
+     * Finds all relationships for a given source entity matching a specified relationshipType in the realm of a
+     * given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param sourceType the entity reference relationshipType
      * @param sourceUrn the reference entity's system-assigned URN
      * @param relationshipType the relationship relationshipType
      * @param page the number of the results page
      * @param size the size of a results page
-     * @return a list of matching {@link RelationshipResponse} instances for the retrieved relationships
+     * @return a page of matching {@link RelationshipResponse} instances for the retrieved relationships
      */
     Page<RelationshipResponse> findByType(
         String tenantUrn,
@@ -101,15 +101,16 @@ public interface RelationshipDao {
         Integer size);
 
     /**
-     * Finds all reverse relationships related to a given reference entity matching a specified relationshipType in the realm of a given account.
+     * Finds all relationships related to a given target entity matching a specified relationshipType in the realm of a
+     * given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param targetType the related entity reference relationshipType
      * @param targetUrn the related reference entity's system-assigned URN
      * @param relationshipType the relationship relationshipType
      * @param page the number of the results page
      * @param size the size of a results page
-     * @return a list of matching {@link RelationshipResponse} instances for the retrieved relationships
+     * @return a page of matching {@link RelationshipResponse} instances for the retrieved relationships
      */
     Page<RelationshipResponse> findByTypeReverse(
         String tenantUrn,
@@ -120,14 +121,14 @@ public interface RelationshipDao {
         Integer size);
 
     /**
-     * Finds all relationships for a given reference entity in the realm of a given account.
+     * Finds all relationships for a given source entity in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param sourceType the entity reference relationshipType
      * @param sourceUrn the reference entity's system-assigned URN
      * @param page the number of the results page
      * @param size the size of a results page
-     * @return a list of matching {@link RelationshipResponse} instances for the retrieved relationships
+     * @return a page of matching {@link RelationshipResponse} instances for the retrieved relationships
      */
     Page<RelationshipResponse> findAll(
         String tenantUrn,
@@ -137,15 +138,14 @@ public interface RelationshipDao {
         Integer size);
 
     /**
-     * Finds all relationships for a given reference entity in the realm of a given account and includes a
-     * {@code reciprocal} flag in the response if {@code checkReciprocal} is set to {@code true}.
+     * Finds all relationships for a given target entity in the realm of a given tenant.
      *
-     * @param tenantUrn the account URN
+     * @param tenantUrn the tenant URN
      * @param targetType the related entity reference relationshipType
      * @param targetUrn the related reference entity's system-assigned URN
      * @param page the number of the results page
      * @param size the size of a results page
-     * @return a list of matching {@link RelationshipResponse} instances for the retrieved relationships
+     * @return a page of matching {@link RelationshipResponse} instances for the retrieved relationships
      */
     Page<RelationshipResponse> findAllReverse(
         String tenantUrn,
